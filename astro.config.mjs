33fedf21e57e7f87eca2d77e363efa49b1fae34d
bobpulaski <82018695+bobpulaski@users.noreply.github.com>
2024-08-19 12:59:05 +0300

    job 19-08-2024

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://bobpulaski.github.io",
-  base: "/",
+  base: "/opb158",
   integrations: [mdx(), sitemap()],
 });
