f00ba7d4c48fea8898ff38cdbc209916d1e9d320
bobpulaski <82018695+bobpulaski@users.noreply.github.com>
2024-08-22 15:24:43 +0300

    add logo, main menu gose to right --job-22-08-2024

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "ОПБ";
+export const SITE_TITLE = "Обеспечение пожарной безопасности";
 export const SITE_DESCRIPTION = "Welcome to my website!";
